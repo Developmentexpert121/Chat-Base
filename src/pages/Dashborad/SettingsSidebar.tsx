858d878c7152b4e175438ab77693961fa7046a3a
Devex Hub <54986861+Developmentexpert121@users.noreply.github.com>
2024-04-04 13:15:56 +0530

    update fields by use hook form, api integration

--- src/pages/Dashborad/SettingsSidebar.tsx
+++ src/pages/Dashborad/SettingsSidebar.tsx
@@ -1,13 +1,15 @@
 import {
   Box,
   Card,
-  Table,
-  TableBody,
+  //Table,
+  //TableBody,
   // TableCell,
   // TableRow,
   Typography,
   Button,
   Grid,
+  FormControlLabel,
+  FormHelperText,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import InputLabel from "@mui/material/InputLabel";
@@ -15,45 +17,53 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
-import { addSettingsData } from "../../services/slices/auth/dashboard-data.tsx";
+import {
+  addSettingsData,
+  scheduleCronSettings,
+  stopCronSettings,
+} from "../../services/slices/auth/dashboard-data.tsx";
 import { useDispatch, useSelector } from "react-redux";
-// import { getUsersConversation } from "../../services/slices/auth/users-conversation.tsx";
+
+import { useForm, Controller } from "react-hook-form";
+import { TextField } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("Email is required"),
+  hours: yup.string().required("Hours is required"),
+  daysOfWeek: yup
+    .array()
+    .of(yup.boolean())
+    .test(
+      "atLeastOneSelected",
+      "Please select at least one day",
+      (value: any) => {
+        return value.some((day: any) => day === true);
+      }
+    ),
+});
+
+const defaultValues = {
+  email: "",
+  hours: "",
+  daysOfWeek: [],
+};
+
+interface FormData {
+  email: string;
+  hours: string;
+  daysOfWeek: any;
+}
 
 const SettingsSidebar = () => {
   const dispatch: any = useDispatch<any>();
-  //   useEffect(() => {
-  //     const chatbotId = localStorhours.getItem("chatbotId");
-  //     const data = {
-  //       chatbotId: chatbotId,
-  //       startDate: "2024-03-12",
-  //       endDate: "2024-12-12",
-  //       phours: 1,
-  //       size: 20,
-  //     };
-  //     dispatch(getUsersConversation(data));
-  //   }, [dispatch]);
-
-  //   const conversationData = useSelector(
-  //     (state: any) => state.usersConversation.data
-  //   );
-  //   const handleChatSelect = (id: any) => {
-
-  //   };
-
-  //   const chat = conversationData?.data
-  //     ? conversationData?.data.map((item: any) => ({
-  //         name:
-  //           item?.form_submission === null
-  //             ? "Customer"
-  //             : item?.form_submission?.email,
-  //         id: item?.id,
-  //       }))
-  //     : null;
-
   const [hours, setHours] = useState<string | number>("");
   const [open, setOpen] = useState(false);
+  const [stats, setStats] = useState<boolean>(false);
   const [selectedDays, setSelectedDays] = useState<any>([]);
   const numbers = Array.from({ length: 24 }, (_, index) => index + 1);
+
   const handleChange = (event: SelectChangeEvent<typeof hours>) => {
     setHours(event.target.value);
   };
@@ -67,22 +77,43 @@ const SettingsSidebar = () => {
     "Friday",
     "Saturday",
   ];
-  const handleChangeCheckbox = (index: any) => {
-    setSelectedDays((prevSelectedDays: any) => {
-      if (prevSelectedDays.includes(index)) {
-        return prevSelectedDays.filter(
-          (selectedDay: any) => selectedDay !== index
-        );
-      } else {
-        return [...prevSelectedDays, index];
-      }
-    });
+
+  const handleStop = () => {
+    dispatch(stopCronSettings());
   };
 
-  const handleSaveData = () => {
-    const values = { hours: hours, selectedDays: selectedDays };
-    dispatch(addSettingsData({ values: values }));
+  const handleStartCron = () => {
+    setStats(true);
+    const fetchData = async () => {
+      dispatch(scheduleCronSettings());
+    };
+    fetchData();
+    // Set up an interval to call the API every 5 minutes
+    const interval = setInterval(fetchData, 5 * 60 * 1000); // 5 minutes in milliseconds
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
   };
+
+  const {
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues,
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    const indexedData = data.daysOfWeek
+      .map((item: any, index: number) => (item === true ? index : ""))
+      .filter((index: any) => index !== "");
+    const values = { hours: data.hours, selectedDays: indexedData };
+    dispatch(
+      addSettingsData({ values: values, emailTo: data.email, status: stats })
+    );
+  };
+
   return (
     <Box sx={{ px: 4, py: 4 }} className="content-height">
       <Grid container spacing={3} className="mb-3">
@@ -113,39 +144,108 @@ const SettingsSidebar = () => {
         }}
       >
         <Box sx={{ flex: "100%" }}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 1, ml: 4 }}>
-            Select Hours
-          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2}>
+              {/* <Grid item xs={4}>
+              <Typography variant="h6" gutterBottom sx={{ mt: 1, ml: 4 }}>
+                Select Hours
+              </Typography>
 
-          <FormControl sx={{ ml: 4, minWidth: 300 }}>
-            <InputLabel id="demo-controlled-open-select-label">
-              Hours
-            </InputLabel>
-            <Select
-              labelId="demo-controlled-open-select-label"
-              id="demo-controlled-open-select"
-              open={open}
-              onClose={() => setOpen(false)}
-              onOpen={() => setOpen(true)}
-              value={hours}
-              label="Hours"
-              onChange={handleChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {numbers.map((number: any) => (
-                <MenuItem key={number} value={number}>
-                  {number}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+              <FormControl sx={{ ml: 4, minWidth: 300 }}>
+                <InputLabel id="demo-controlled-open-select-label">
+                  Hours
+                </InputLabel>
+                <Select
+                  labelId="demo-controlled-open-select-label"
+                  id="demo-controlled-open-select"
+                  open={open}
+                  onClose={() => setOpen(false)}
+                  onOpen={() => setOpen(true)}
+                  value={hours}
+                  label="Hours"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  {numbers.map((number: any) => (
+                    <MenuItem key={number} value={number}>
+                      {number}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid> */}
+              <Grid item xs={4}>
+                <Typography variant="h6" gutterBottom sx={{ mt: 1, ml: 4 }}>
+                  Select Hours
+                </Typography>
 
-          <Typography variant="h6" gutterBottom sx={{ mt: 1, ml: 4 }}>
-            Select Days
-          </Typography>
-          <Grid container spacing={2} sx={{ ml: 1 }}>
+                <Controller
+                  name="hours"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <FormControl sx={{ ml: 4, minWidth: 300 }}>
+                      <InputLabel id="demo-controlled-open-select-label">
+                        Hours
+                      </InputLabel>
+                      <Select
+                        {...field}
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        open={open}
+                        onClose={() => setOpen(false)}
+                        onOpen={() => setOpen(true)}
+                        label="Hours"
+                        error={!!errors.hours}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        {numbers.map((number: any) => (
+                          <MenuItem key={number} value={number}>
+                            {number}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                      {errors.hours && (
+                        <FormHelperText error>
+                          {errors.hours.message}
+                        </FormHelperText>
+                      )}
+                    </FormControl>
+                  )}
+                />
+              </Grid>
+
+              <Grid item xs={4}>
+                <Typography variant="h6" gutterBottom sx={{ mt: 1, ml: 4 }}>
+                  Email
+                </Typography>
+
+                <FormControl sx={{ ml: 4, minWidth: 300 }}>
+                  <Controller
+                    name="email"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Email"
+                        error={!!errors.email}
+                        helperText={errors.email?.message}
+                      />
+                    )}
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+
+            <Typography variant="h6" gutterBottom sx={{ mt: 1, ml: 4 }}>
+              Select Days
+            </Typography>
+            {/* <Grid container spacing={2} sx={{ ml: 1 }}>
             {daysOfWeek.map((day: any, index: number) => (
               <Grid item xs={4} key={index}>
                 <Checkbox
@@ -156,29 +256,77 @@ const SettingsSidebar = () => {
                 {day}
               </Grid>
             ))}
-          </Grid>
+          </Grid> */}
 
-          <Grid container spacing={2} sx={{ mt: 2, ml: 2 }}>
-            <Grid item xs={8}>
-              <Button
-                variant="contained"
-                color="success"
-                sx={{ width: 150 }}
-                onClick={() => handleSaveData()}
+            <Grid container spacing={2} sx={{ ml: 1 }}>
+              {daysOfWeek.map((day: any, index: number) => (
+                <Grid item xs={4} key={index}>
+                  <FormControl
+                    component="fieldset"
+                    error={!!errors.daysOfWeek?.root}
+                  >
+                    <Controller
+                      name={`daysOfWeek.${index}`}
+                      control={control}
+                      defaultValue={false}
+                      render={({ field }) => (
+                        <FormControlLabel
+                          control={<Checkbox {...field} />}
+                          label={day}
+                        />
+                      )}
+                    />
+                    {index === 6 &&
+                      errors.daysOfWeek &&
+                      errors.daysOfWeek.root &&
+                      !watch("daysOfWeek")?.find(
+                        (item: any) => item === true
+                      ) && (
+                        <FormHelperText>
+                          {errors.daysOfWeek?.root?.message}
+                        </FormHelperText>
+                      )}
+                  </FormControl>
+                </Grid>
+              ))}
+            </Grid>
+
+            <Grid container spacing={2} sx={{ mt: 2, ml: 2 }}>
+              <Grid item xs={8}>
+                <Button
+                  variant="contained"
+                  color="success"
+                  sx={{ width: 150 }}
+                  // onClick={() => handleSaveData()}
+                  type="submit"
+                >
+                  Save
+                </Button>
+              </Grid>
+              <Grid
+                item
+                xs={4}
+                sx={{ display: "flex", justifyContent: "flex-center" }}
               >
-                Save
-              </Button>
+                <Button
+                  variant="contained"
+                  color="success"
+                  sx={{ width: 100, mr: 4 }}
+                  onClick={() => handleStartCron()}
+                >
+                  Start
+                </Button>
+                <Button
+                  variant="contained"
+                  color="error"
+                  sx={{ width: 100 }}
+                  onClick={() => handleStop()}
+                >
+                  Stop
+                </Button>
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={4}
-              sx={{ display: "flex", justifyContent: "flex-center" }}
-            >
-              <Button variant="contained" color="error" sx={{ width: 150 }}>
-                Stop
-              </Button>
-            </Grid>
-          </Grid>
+          </form>
         </Box>
       </Card>
     </Box>
